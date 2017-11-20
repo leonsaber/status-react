70bc5fc66d479c11492ea184094b0feef7e3d015
Eric Dvorsak <eric@dvorsak.fr>
2017-11-20 23:24:07 +0100

    [feature] refactor discover events
    use re-frame idiomatic register-handler-fx, reg-fx and coeffect.
    the events are still not free of side-effects as there is some
    db calls involved which will be taken care of in a future PR
    
    it also reduces the events fired at app init from 3 to 1
    discover-related event

--- src/status_im/ui/screens/db.cljs
+++ src/status_im/ui/screens/db.cljs
@@ -152,20 +152,20 @@
                   :chat/chat-ui-props
                   :chat/chat-list-ui-props
                   :chat/layout-height
-                  :chat/expandable-view-height-to-value 
+                  :chat/expandable-view-height-to-value
                   :chat/loading-allowed
                   :chat/message-data
                   :chat/message-id->transaction-id
                   :chat/message-status
                   :chat/unviewed-messages
                   :chat/selected-participants
-                  :chat/chat-loaded-callbacks 
+                  :chat/chat-loaded-callbacks
                   :chat/command-hash-valid?
                   :chat/public-group-topic
                   :chat/confirmation-code-sms-listener
                   :chat/messages
                   :chat/handler-data
-                  :chat/loaded-chats 
+                  :chat/loaded-chats
                   :chat/raw-unviewed-messages
                   :chat/bot-db
                   :chat/geolocation
@@ -176,7 +176,6 @@
                   :discoveries/tags
                   :discoveries/current-tag
                   :discoveries/request-discoveries-timer
-                  :discoveries/new-discover
                   :wallet/wallet
                   :wallet/wallet.transactions
                   :prices/prices
